51ce1690f02ad6ec49607f615110565708f25f96
Marcus Alvarez <malvarez@mednet.ucla.edu>
2020-08-05 11:52:18 -0700

    format pct barplot

--- R/barplots.R
+++ R/barplots.R
@@ -10,10 +10,16 @@
 pct_stack_barplt <- function(datf, x, y, fill){
     require(ggplot2)
     require(reshape2)
+    require(scales)
 
     datfm <- melt(datf)
     p <- ggplot(datf, aes_string(x = x, y = y, fill = fill)) + 
-      geom_bar(position = "fill", stat = "identity")
+      geom_bar(position = "fill", stat = "identity") + 
+      scale_y_continuous(labels=percent) + 
+      theme_bw() + 
+      theme(axis.text.x = element_text(angle = 30, hjust = 0.9), 
+            panel.border = element_blank(), 
+            axis.line = element_line())
 
     return(p)
 }
